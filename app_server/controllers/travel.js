04e9eff6c0b3a7f878081fb52ccf2c610242fd21
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-07-30 01:08:44 -0500

    travel handlecards hardcode to JSON
    hardcode replaced with JSON in travel handlebars

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+var fs = require('fs'); 
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // GET travel view
 const travel = (req, res) => { 
     pageTitle = process.env.npm_packages_websiteTitle + ' - Travel';
-    res.render('travel', { title: pageTitle}); 
+    res.render('travel', { title: pageTitle, trips}); 
 };
 
 module.exports = {
