7c2498433cb81758c94885952ec2a14972de827e
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-08-09 11:21:18 -0500

    modified site to use API endpoints
    modified site to use API endpoints, erased and added new functions in server travel controller

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,56 @@
-var fs = require('fs'); 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    //TODO: add relative server later
+    server: 'http://localhost:3000'
+};
 
-// GET travel view
-const travel = (req, res) => { 
-    pageTitle = process.env.npm_packages_websiteTitle + ' - Travel';
-    res.render('travel', { title: pageTitle, trips}); 
+//TODO: optional: create renderTravelDetails and travelDetails based on methods below
+
+// render travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist within our database';
+        }
+    }
+    res.render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+    );
+}
+
+
+// GET travel list view
+const travelList = (req, res) => { 
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {}
+    };
+
+    // console log vs console info: https://dev.to/fatematzuhora/different-use-cases-of-console-log-you-should-use-when-debugging-javascript-30pf
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
 };
 
 module.exports = {
-    travel
+    travelList
 };
