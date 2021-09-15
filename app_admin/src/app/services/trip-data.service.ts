c56c16172099f4730a638bc2f69984020d274f8e
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-09-15 18:35:50 -0500

    resolved known promise error, however, site will not fully load
    IMPORTANT! resolved promise.then issues... However site does not fully load. compiler shows no errors, but errors are viewable in website console log... trouble connecting with api/trips. I am probably guessing the issues is still with the promise or the trip & message declaration in the TS-trip-listing component

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Trip } from '../models/trip';
 
 
@@ -9,14 +10,15 @@ export class TripDataService {
   constructor(private http: HttpClient) { };
 
   private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripURL = `${this.apiBaseUrl}trips/`
 
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     //let URI = `${this.apiBaseUrl}/trips/`;
     return this.http //.get<Trip>(URI)
-      .get(`${this.apiBaseUrl}trips`)
+      .get(this.tripURL)
       .toPromise()
-      .then(response => response.json() as Trip[])
+      .then(response => response as Trip[])
       .catch(this.handleError);
   }
 
